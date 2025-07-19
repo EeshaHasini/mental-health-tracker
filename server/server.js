89f04c6321d271d8e7d57a558b060051e5115d4c
Eesha Hasini <eesha.hasini.007@gmail.com>
2025-07-19 11:40:26 +0530

    Save changes before switching to main

--- server/server.js
+++ server/server.js
@@ -3,32 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+const protectedRoutes = require('./routes/protected');
+const journalRoutes = require('./routes/journals');
+const moodRoutes = require('./routes/moods');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://EeshaHasini:eesha2007@cluster0.mupjk3x.mongodb.net/ekamind?retryWrites=true&w=majority&appName=Cluster0', {
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch((err) => console.error('❌ MongoDB connection error:', err.message));
 
 app.get('/', (req, res) => {
-  res.send('✅ Server is up');
+  res.send('✅ Server is up and running');
 });
 
 app.use('/api/auth', authRoutes);
-console.log('✅ authRoutes loaded');
-
-const protectedRoutes = require('./routes/protected');
 app.use('/api', protectedRoutes);
-
-const journalRoutes = require('./routes/journals');
 app.use('/api/journals', journalRoutes);
-
-const moodRoutes = require('./routes/moods');
 app.use('/api/moods', moodRoutes);
 
 app.listen(PORT, () => {
