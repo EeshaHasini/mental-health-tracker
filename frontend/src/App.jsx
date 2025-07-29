82a3111d1c2f815f50944142f6ef06390a1d7f03
Akshitha Muttangi <akshithamuthangi@gmail.com>
2025-07-29 20:49:12 +0530

    copied

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import Profile from './Components/Profile';
+
+
+function App() {
+
+
+  return (
+      <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
